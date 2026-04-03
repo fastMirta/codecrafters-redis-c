eb01f684e1a64fb5ceb5e072e0e4906d5f0dd5e4
fast mirta <tamir.szewach@gmail.com>
2026-04-03 17:59:37 +0300

    Changed time implmentation, changed store_set logic and removed unnecessary store_set_flags functions

--- include/utils.h
+++ include/utils.h
@@ -12,14 +12,12 @@ typedef struct Entry {
     char *key;
     char *value;
     struct Entry *next;
-    time_t expires_at;
+    long long expires_at;
 } Entry;
 
 extern Entry *table[TABLE_SIZE];
 
 int hash(char *key);
-void store_set_EX(char *key, int exTime);
-void store_set_PX(char *key, int pxTime);
 void store_set(char *key, char *value, TIME_FLAGS flag, int seconds);
 char* store_get(char *key);
 
--- src/utils.c
+++ src/utils.c
@@ -1,8 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "utils.h"
+#include <sys/time.h>
 
 Entry *table[TABLE_SIZE] = {NULL};
+
+long long get_current_time_ms() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (long long)(tv.tv_sec) * 1000 + (tv.tv_usec / 1000);
+}
 
 int hash(char *key) {
     int hash = 0;
@@ -11,35 +18,28 @@ int hash(char *key) {
     return hash;
 }
 
-void store_set_EX(char *key, int exTime){
-    int index = hash(key);
-    table[index]->key = key;
-    table[index]->expires_at = 0; 
-    
-    table[index]->expires_at = time(NULL) + exTime;
-}
-
-void store_set_PX(char *key, int pxTime){
-    int index = hash(key);
-    table[index]->key = key;
-    table[index]->expires_at = 0; 
-    
-    table[index]->expires_at = time(NULL) + (pxTime/1000);
-}
 
 void store_set(char *key, char *value, TIME_FLAGS flag, int seconds) {
     int index = hash(key);
     if (table[index] == NULL)
         table[index] = malloc(sizeof(Entry));
+    //Clean old memory
+    table[index]->key = strdup(key); 
+    table[index]->value = strdup(value);
+    
     table[index]->key = key;
     table[index]->value = value;
+    table[index]->expires_at = 0;
 
-    if(seconds == -1)
-        table[index]->expires_at = 0;
-    else if(flag == EX)
-        store_set_EX(key, seconds);
-    else if(flag == PX)
-        store_set_PX(key, seconds);
+    if (seconds != -1) {
+        long long now = get_current_time_ms();
+        if (flag == EX) {
+            table[index]->expires_at = now + ((long long)seconds * 1000);
+        } else if (flag == PX) {
+            table[index]->expires_at = now + (long long)seconds;
+        }
+    }
+
     printf("\n");
     printf("seconds: %d\n", table[index]->expires_at);
 }
@@ -48,10 +48,13 @@ char *store_get(char *key) {
     int index = hash(key);
     if (table[index] == NULL)
         return NULL;
-    if (table[index]->expires_at != 0 && time(NULL) > table[index]->expires_at) {
-        free(table[index]);
-        table[index] = NULL;
-        return NULL;
+
+    if (table[index]->expires_at != 0) {
+        long long now = get_current_time_ms();
+        if (now >= table[index]->expires_at) {
+            table[index] = NULL; 
+            return NULL; 
+        }
     }
     return table[index]->value;
 }
