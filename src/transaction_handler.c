b2463ebd36933f8f1257ed3f1c63f09bd0cdd91c
fast mirta <tamir.szewach@gmail.com>
2026-04-09 07:05:50 +0300

    Fixed EXEC after EXEC response

--- src/transaction_handler.c
+++ src/transaction_handler.c
@@ -70,13 +70,13 @@ void handle_exec(Client *client){
     char header[32];
     int headerLen = snprintf(header, sizeof(header), "*%d\r\n", client->queuedCommands);
     send(client->fd, header, headerLen, 0);
+    client->is_queued = 0;
 
     if(client->queuedCommands == 0){
-        //send(client->fd, "*0\r\n", 3, 0);
         return;
     }
 
-    client->is_queued = 0;
+    
     printf("queued cmds: %d\n", client->queuedCommands);
     for(int i = 0; i < client->queuedCommands; i++){
         printf("Entered loop in exec\n");
