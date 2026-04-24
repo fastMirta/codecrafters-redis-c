33c48366ca5ffb293ba7b0a4e9f3d94e06021fbb
fast mirta <tamir.szewach@gmail.com>
2026-04-24 14:54:59 +0300

    added hardcode answer to acl getuser

--- src/auth_handler.c
+++ src/auth_handler.c
@@ -6,4 +6,8 @@ void handle_acl(RespRequest *req, int client_fd){
         send(client_fd, "$7\r\ndefault\r\n", 13, 0);
         return;
     }
+    if(strcmp(req->args[0], "GETUSER") == 0 && (strcmp(req->args[1], "default") == 0 || strcmp(req->args[1], "DEFAULT") == 0)){
+        send(client_fd, "*2\r\n$5\r\nflags\r\n*0\r\n", 19, 0);
+        return;
+    }
 }
