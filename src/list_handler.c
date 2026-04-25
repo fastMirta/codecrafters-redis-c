0587a1c3d680fdfa4be4c7939e88ad0d933d6047
fast mirta <tamir.szewach@gmail.com>
2026-04-25 20:29:01 +0300

    Fixed lpush not adding to the start and fixed bad logic in rpush (using list values b4 checking for null)

--- src/list_handler.c
+++ src/list_handler.c
@@ -10,17 +10,23 @@ void handle_rpush(RespRequest *req, int client_fd){
     Entry *entry = store_getEntry(req->args[0]);
     if(entry == NULL || entry->value == NULL){
         list = calloc(1, sizeof(List));
-        list->size = 0;
-        list->capacity = req->argc - 1;
-        list->values = malloc(list->capacity * sizeof(char *));
-
-
-        store_set_list(req->args[0], list);
-        //ist->capacity
+        
         if(list == NULL){
             send(client_fd, "-ERR Out of memory\r\n", 20, 0);
             return;
         }
+
+        list->size = 0;
+        list->capacity = req->argc - 1;
+        list->values = malloc(list->capacity * sizeof(char *));
+
+        if(list->values == NULL){
+            send(client_fd, "-ERR Out of memory\r\n", 20, 0);
+            return;
+        }
+
+
+        store_set_list(req->args[0], list);
     }
     else{
         list = (List*)(entry->value);
@@ -42,43 +48,43 @@ void handle_rpush(RespRequest *req, int client_fd){
 
 void handle_lpush(RespRequest *req, int client_fd){
     if(req->argc < 2){
-        send(client_fd, "-ERR wrong number of arguments for 'rpush' command\r\n", 52, 0);
+        send(client_fd, "-ERR wrong number of arguments for 'lpush' command\r\n", 52, 0);
         return;
     }
-
     List *list = NULL;
     Entry *entry = store_getEntry(req->args[0]);
+    int newCount = req->argc - 1;
+
     if(entry == NULL || entry->value == NULL){
         list = calloc(1, sizeof(List));
         list->size = 0;
-        list->capacity = req->argc - 1;
+        list->capacity = newCount;
         list->values = malloc(list->capacity * sizeof(char *));
-
-
         store_set_list(req->args[0], list);
-        //ist->capacity
         if(list == NULL){
             send(client_fd, "-ERR Out of memory\r\n", 20, 0);
             return;
         }
-    }
-    else{
+    } else {
         list = (List*)(entry->value);
-        list->values = realloc(list->values, (list->size + req->argc - 1) * sizeof(char *));
+        list->values = realloc(list->values, (list->size + newCount) * sizeof(char *));
     }
 
-    for(int i = req->argc - 1; i > 0; i--){
-        list->values[list->size] = strdup(req->args[i]);
-        list->size++;
+    // shift existing elements right
+    for(int i = list->size - 1; i >= 0; i--){
+        list->values[i + newCount] = list->values[i];
     }
-    
+    // prepend in reverse order
+    for(int i = 1; i <= newCount; i++){
+        list->values[newCount - i] = strdup(req->args[i]);
+    }
+    list->size += newCount;
+
     char addMsg[1024];
     snprintf(addMsg, sizeof(addMsg), ":%zd\r\n", list->size);
-
-    printf("addMsg: %s\n", addMsg);
     send(client_fd, addMsg, strlen(addMsg), 0);
-    
 }
+
 
 void handle_lrange(RespRequest *req, int client_fd){
     if(req->argc < 3) return;
